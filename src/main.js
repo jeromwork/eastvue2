f9357e2bf5900682680a59c36d5f91cf13ea96d5
jeromwork <jeromwork@inbox.ru>
2021-03-28 08:49:17 +0300

    повторный комит

--- src/main.js
+++ src/main.js
@@ -1,8 +1,19 @@
 import Vue from 'vue'
 import App from './App.vue'
+import store from './store'
+import vuetify from './plugins/vuetify' // path to vuetify export
+
+
 
 Vue.config.productionTip = false
 
+//Vue.use(Vuetify)
+
+
 new Vue({
+  store,
+  vuetify,
   render: h => h(App),
 }).$mount('#app')
+
+
